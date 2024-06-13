20a8a9d05131faacd65377df9c52c8758d3e2598
ivaniv_maks <ivanivmaks0@gmail.com>
2024-06-13 14:25:33 +0300

    updated

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,36 +1,27 @@
-import { Formik, Form, Field } from "formik";
-import { useId } from "react";
-import css from "./SearchBox.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { selectFilter } from "../../redux/filters/selectors";
 import { changeFilter } from "../../redux/filters/slice";
+import css from "./SearchBox.module.css";
 
 export default function SearchBox() {
- const value = useSelector(selectFilter);
+  const value = useSelector(selectFilter);
   const dispatch = useDispatch();
-  
-  const handleFilter = (event) => {
-   const normalizedValue = event.target.value.toLowerCase();
 
-   dispatch(changeFilter(normalizedValue));
+  const onChange = (event) => {
+    const normalizedValue = event.target.value.toLowerCase();
+
+    dispatch(changeFilter(normalizedValue));
   };
 
-  const filterId = useId();
   return (
     <div>
-      <Formik initialValues={value} onSubmit={() => {}}>
-        <Form>
-          <label htmlFor={filterId}> Find contacts by name:</label>
-          <Field
-            type="text"
-            name="filter"
-            id={filterId}
-            className={css.filter}
-            value={value}
-            onChange={handleFilter}
-          />
-        </Form>
-      </Formik>
+      <label>Find contacts by name:</label>
+      <input
+        className={css.filter}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
--- src/redux/filters/slice.js
+++ src/redux/filters/slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const slice = createSlice({
-  name: "filters",
+  name: "filter",
   initialState: "",
   reducers: {
     changeFilter: (state, action) => {
